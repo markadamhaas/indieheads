62aa985a111331f540d21b287e94ca5d7bf3b266
mah3fw <89609654+mah3fw@users.noreply.github.com>
2023-07-14 15:19:12 -0400

    seperated forms from views, fixed typo

---
+++ forms.py
@@ -0,0 +1,32 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, DateField, IntegerField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+
+class CreateShowForm(FlaskForm):
+    # Event Record
+    venue = SelectField('Venue', validators=[DataRequired()])
+    date = DateField('Date', format='%Y-%m-%d', validators=[DataRequired()])
+    time = StringField('Time', validators=[DataRequired()])
+    ticketprice = IntegerField('Ticket Price', validators=[DataRequired()])
+
+    # Equipment Select
+
+    # Merch Select
+
+    # Set List
+
+    # Volunteer Schedule
+
+    # Submit
+    submit = SubmitField('Create')
+
+class CreateBandForm(FlaskForm):
+    # Event Record
+    name = StringField('Band', validators=[DataRequired()])
+    genre = StringField('Genre')
+    Instagram = StringField('Instagram')
+    contact = StringField('Contact Name')
+    contactphone = StringField('Contact Phone Number')
+    
+    # Submit
+    submit = SubmitField('Create')
