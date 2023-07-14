7778d9e0ed0654f54e3fbeb730b111b67374aacd
rjh8eq <112512005+rjh8eq@users.noreply.github.com>
2023-07-13 20:20:33 -0400

    Finished bands add, edit, delete, display

--- run.py
+++ run.py
@@ -1,18 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 app = Flask(__name__)
 
-app.debug = True
-
-
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",  
-  password="password",  
-  database="indieheads" 
+    host="localhost",
+    user="root",  
+    password="password",  
+    database="indieheads" 
 )
-
 
 @app.route("/")
 def index_page():
@@ -20,101 +16,76 @@ def index_page():
 
 @app.route("/bands", methods=['GET', 'POST'])
 def bands():
-    
     cursor = mydb.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM BANDS;")
+    sort_option = request.args.get('sort', 'Band_Name')  # Default sorting is by name
+    cursor.execute(f"SELECT * FROM BANDS ORDER BY {sort_option};")
     bands = cursor.fetchall()
-    
-    band_details = None
-    members = None
-    if 'band_id' in request.args:
-        band_id = request.args.get('band_id')
-        cursor.execute("SELECT * FROM BANDS WHERE Band_ID = %s;", (band_id,))
-        band_details = cursor.fetchone()
-        cursor.execute("SELECT * FROM MEMBERS WHERE Band_ID = %s;", (band_id,))
-        members = cursor.fetchall()
-    
-    search_results = None
-    if request.method == 'POST':
-        query = request.form.get('q')
-        cursor.execute("SELECT * FROM BANDS WHERE Band_Name LIKE %s;", ('%' + query + '%',))
-        search_results = cursor.fetchall()
-    
     cursor.close()
-    
-    return render_template('bands.html', bands=bands, band_details=band_details, members=members, search_results=search_results)
+    return render_template('bands.html', bands=bands, sort=sort_option)
 
-@app.route('/search', methods=['POST'])
-def search():
-    q = request.form.get('q')
-    # Perform the search and store the results in the `bands` variable
-    bands = perform_search_on_bands(q)
-    return render_template('bands.html', bands=bands)
+@app.route("/add_band", methods=['POST'])
+def add_band():
+    cursor = mydb.cursor()
+    newBandName = request.form.get('Band_Name')
+    newBandGenre = request.form.get('Band_Genre')
+    newBandLocation = request.form.get('Band_Location')
+    newBandInstagram = request.form.get('Band_Instagram')
+    newBandContactPerson = request.form.get('B_Contact_Person')
+    newBandContactPhone = request.form.get('B_Contact_Number')
+    cursor.execute("INSERT INTO BANDS (Band_Name, Band_Genre, Band_Location, Band_Instagram, B_Contact_Person, B_Contact_Number) VALUES (%s, %s, %s, %s, %s, %s);", 
+                    (newBandName, newBandGenre, newBandLocation, newBandInstagram, newBandContactPerson, newBandContactPhone))
+    mydb.commit()
+    cursor.close()
+    return redirect(url_for('bands'))
 
-def perform_search_on_bands(q):
+@app.route("/delete_band/<int:id>", methods=['POST'])
+def delete_band(id):
+    cursor = mydb.cursor()
+    cursor.execute("DELETE FROM BANDS WHERE Band_ID = %s;", (id,))
+    mydb.commit()
+    cursor.close()
+    return redirect(url_for('bands'))
+
+@app.route("/edit_band/<int:id>", methods=['GET', 'POST'])
+def edit_band(id):
     cursor = mydb.cursor(dictionary=True)
+    if request.method == 'POST':
+        bandName = request.form.get('Band_Name')
+        bandGenre = request.form.get('Band_Genre')
+        bandLocation = request.form.get('Band_Location')
+        bandInstagram = request.form.get('Band_Instagram')
+        bandContactPerson = request.form.get('B_Contact_Person')
+        bandContactPhone = request.form.get('B_Contact_Number')
+        cursor.execute("UPDATE BANDS SET Band_Name = %s, Band_Genre = %s, Band_Location = %s, Band_Instagram = %s, B_Contact_Person = %s, B_Contact_Number = %s WHERE Band_ID = %s;", 
+                        (bandName, bandGenre, bandLocation, bandInstagram, bandContactPerson, bandContactPhone, id,))
+        mydb.commit()
+        cursor.close()
+        return redirect(url_for('bands'))
+    else:
+        cursor.execute("SELECT * FROM BANDS WHERE Band_ID = %s;", (id,))
+        band = cursor.fetchone()
+        cursor.close()
+        return render_template('edit-band.html', band=band)
 
-    query = f"SELECT * FROM BANDS WHERE Band_Name LIKE '%{q}%'"
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    return results
-
-@app.route('/sort', methods=['POST'])
-def sort():
-    option = request.form.get('sort-option')
-    print(option)
-    # Sort the bands according to the selected option and store the results in the `bands` variable
-    bands = perform_sort_on_bands(option)
-    return render_template('bands.html', bands=bands)
-
-def perform_sort_on_bands(option):
-    cursor = mydb.cursor(dictionary=True)
-    if option == 'name':
-        query = f"SELECT * FROM BANDS ORDER BY Band_Name"
-
-    if option == 'location':
-        query = f"SELECT * FROM BANDS ORDER BY Band_Location"
-
-    if option == 'genre':
-        query = f"SELECT * FROM BANDS ORDER BY Band_Genre"
-    cursor.execute(query)
-    results = cursor.fetchall()
-
-    return results
-
-@app.route('/band_details/<int:id>')
-def band_details(id):
-    
-    cursor = mydb.cursor(dictionary=True)
-    # Fetch band details
-    cursor.execute("SELECT * FROM BANDS WHERE Band_ID = %s;", (id,))
-    band = cursor.fetchone()
-
-    # Fetch band members
-    cursor.execute("SELECT * FROM MEMBERS WHERE Band_ID = %s;", (id,))
-    members = cursor.fetchall()
-
-    return render_template('band-details.html', band=band, members=members)
 
 @app.route('/shows')
 def shows():
-    # Add the code to retrieve shows data from the database and render the template
+    # placeholder
     return render_template('shows.html')
 
 @app.route('/venues')
 def venues():
-    # Add the code to retrieve venues data from the database and render the template
+    # placeholder
     return render_template('venues.html')
 
 @app.route('/equipment')
 def equipment():
-    # Add the code to retrieve equipment data from the database and render the template
+    # placeholder
     return render_template('equipment.html')
 
 @app.route('/merch')
 def merch():
-    # Add the code to retrieve merch data from the database and render the template
+    # placeholder
     return render_template('merch.html')
 
 if __name__ == '__main__':
